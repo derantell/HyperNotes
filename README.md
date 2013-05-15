d44f3c43cb9bbdee040b9ca7e6b33a1f2b45fe68
David Tellander <dtellander@gmail.com>
2013-05-15 22:40:22 +0200

    Some experimeting with Nancy bootstrapping and OWIN hosting

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ HyperNotes
 ==========
 
 A web app with a hypermedia API that lets users store, edit and search markdown documents.
+
+Tech
+----
+
+Nancy - [hosting on owin/katana](http://www.strathweb.com/2012/12/running-aspnet-web-api-with-owin-and-katana/)
+
+RavenDB - Run server in ram
---
+++ src/HyperNotes.Api/UserModule.cs
@@ -0,0 +1,56 @@
+﻿using Nancy;
+using Nancy.Bootstrapper;
+using Nancy.ModelBinding;
+using Nancy.TinyIoc;
+using Owin;
+using Raven.Client;
+using Raven.Client.Document;
+
+namespace HyperNotes.Api {
+    public class UserModule : NancyModule {
+        public UserModule() {
+            Get["/"] = _ => "Hello user!";
+
+            Put["/users"] = data => {
+                var user = this.Bind<UserModel>();
+
+                using (var db = Raven.Db.OpenSession()) {
+                    db.Store(user);
+                    db.SaveChanges();
+                }
+
+                return 201;
+            };
+        }
+    }
+
+    public class UserModel {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class HyperNoteBootstrapper : DefaultNancyBootstrapper {
+        protected override void ApplicationStartup(TinyIoCContainer container, IPipelines pipelines)
+        {
+            base.ApplicationStartup(container, pipelines);
+
+            Raven.Init("http://derantell-pc:8080");
+        }
+    }
+
+    public class Startup {
+        public void Configuration(IAppBuilder app) {
+            app.UseNancy();
+        }
+    }
+
+    public static class Raven {
+        public static IDocumentStore Db { get; private set; }
+
+        public static void Init(string url) {
+            Db = new DocumentStore {Url = url};
+            Db.Initialize();
+        }
+    }
+}
