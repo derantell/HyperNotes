da3469dd35516c8659150a3b908675ccf3eda3e5
David Tellander <dtellander@gmail.com>
2013-08-07 21:05:06 +0200

    Added some more tests for CollectionBuilder

--- test/HyperNotes.CollectionJson.Test/CollectionBuilderTest.cs
+++ test/HyperNotes.CollectionJson.Test/CollectionBuilderTest.cs
@@ -1,23 +1,67 @@
 ﻿using System;
+using System.Linq;
 using FluentAssertions;
 using Ploeh.AutoFixture;
 using Xunit;
 
 namespace HyperNotes.CollectionJson.Test {
     public class CollectionBuilderTest {
-        public CollectionBuilderTest() {
-            collectionBuilder = DefineCollection.For<TestModel>(CollectionHref);
-        }
-
+        
         public class Build_method {
+            private Fixture _fixture;
+
+            public Build_method() {
+                _fixture = new Fixture();
+            }
+
             [Fact]
-            public void should_return_a_new_collectionJson_instance() {
-                var collection = collectionBuilder
+            public void should_return_a_new_collectionJson_instance_with_specified_href() {
+                var builder = DefineCollection.For<TestModel>(CollectionHref);
+                var model = _fixture.Create<TestModel>();
+
+                var doc = builder.Build(new[]{model});
+
+                doc.collection.href.Should().Be(CollectionHref);
+                doc.collection.version.Should().Be("1.0");
             }
         }
 
+        public class Links_method {
+            private Fixture _fixture;
+
+            public Links_method() {
+                _fixture = new Fixture();
+            }
+
+            [Fact]
+            public void should_add_links_to_the_collection() {
+                var builder = DefineCollection.For<TestModel>(CollectionHref);
+                var model = _fixture.Create<TestModel>();
+                var links = _fixture.CreateMany<Link>(3).ToArray();
+
+                var doc = builder
+                    .Links(links)
+                    .Build(new[]{model});
+
+                doc.collection.links.ShouldBeEquivalentTo(links);
+            }
+        }
+
+
+        public class Items_method {
+            private Fixture _fixture;
+
+            public Items_method() {
+                _fixture = new Fixture();
+            }
+
+            [Fact]
+            public void should_add_generators_to_builder() {
+                var builder = DefineCollection.For<TestModel>( CollectionHref);
+                var models = _fixture.CreateMany<TestModel>();
+            }
+        }
         private const string CollectionHref = "http://example.com/collection";
-        private CollectionBuilder<TestModel> collectionBuilder;
     }
 
     public class DefineCollectionTest {
