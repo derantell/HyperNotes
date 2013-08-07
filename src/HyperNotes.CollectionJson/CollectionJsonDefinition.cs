9a06524bff374defdff0d75d726fdf286fdf707d
David Tellander <dtellander@gmail.com>
2013-08-07 20:17:59 +0200

    Added test projects; tests for CollectionBuilder

--- src/HyperNotes.CollectionJson/CollectionJsonDefinition.cs
+++ src/HyperNotes.CollectionJson/CollectionJsonDefinition.cs
@@ -15,84 +15,6 @@ namespace HyperNotes.CollectionJson {
 
         protected abstract CollectionBuilder<TModel> GetBuilder();
 
-        protected static class DefineCollection {
-            public static CollectionBuilder<TModel> For<TModel>(string href, string version = "1.0") {
-                return new CollectionBuilder<TModel>( href, version );
-            }
-        }
-        
-        protected class CollectionBuilder<TModel> {
-            public CollectionJson Build(IEnumerable<TModel> model) {
-                var collection = new Collection {
-                    href = _href,
-                    version = _version,
-                };
-
-                if (_links != null) {
-                    collection.links = _links;
-                }
-
-                if (model.Any()) {
-                    collection.items = model.Select(m => new Item {
-                        href = _itemHrefGenerator(m),
-                        links = _itemLinkGenerator(m),
-                        data = _itemDataGenerator(m)
-                    });
-                }
-
-                if (_templateData != null) {
-                    collection.template = new Template {data = _templateData};
-                }
-                
-                if (_queries != null) {
-                    collection.queries = _queries;
-                }
-
-                return new CollectionJson {collection = collection};
-            }
-
-
-            public CollectionBuilder<TModel> Links(params Link[] links) {
-                _links = links;
-                return this;
-            } 
-
-            public CollectionBuilder<TModel> Items(
-                Func<TModel, string> href, 
-                Func<TModel, IEnumerable<Data>> data = null, 
-                Func<TModel, IEnumerable<Link>> links = null ) 
-            {
-                _itemHrefGenerator = href ?? (_ => null);
-                _itemDataGenerator = data ?? (_ => null);
-                _itemLinkGenerator = links ?? (_ => null);
-                return this;
-            } 
-
-            public CollectionBuilder<TModel> Template(params Data[] data) {
-                _templateData = data;
-                return this;
-            } 
-
-            public CollectionBuilder<TModel> Queries(params Query[] queries) {
-                _queries = queries;
-                return this;
-            }
-
-            public CollectionBuilder(string href, string version) {
-                _href = href;
-                _version = version;
-            }
-
-            private string _href;
-            private string _version;
-            private IEnumerable<Link> _links;
-            private Func<TModel, string> _itemHrefGenerator = (_ => null);
-            private Func<TModel, IEnumerable<Data>> _itemDataGenerator = (_ => null);
-            private Func<TModel, IEnumerable<Link>> _itemLinkGenerator = (_ => null);
-            private IEnumerable<Data> _templateData;
-            private IEnumerable<Query> _queries;
-        }
-
         protected static Data Data(string name, string value = null, string prompt = null) {
             return new Data {name = name, value = value, prompt = prompt};
         }
@@ -104,5 +26,83 @@ namespace HyperNotes.CollectionJson {
         protected static Query Query(string rel, string href, string name = null, string prompt = null, IEnumerable<Data> data = null) {
             return new Query { rel = rel, href = href, name = name, prompt = prompt, data = data };
         }
-   }
+    }
+
+    public static class DefineCollection {
+        public static CollectionBuilder<TModel> For<TModel>(string href, string version = "1.0") {
+            return new CollectionBuilder<TModel>( href, version );
+        }
+    }
+    
+    public class CollectionBuilder<TModel> {
+        public CollectionJson Build(IEnumerable<TModel> model) {
+            var collection = new Collection {
+                href = _href,
+                version = _version,
+            };
+
+            if (_links != null) {
+                collection.links = _links;
+            }
+
+            if (model.Any()) {
+                collection.items = model.Select(m => new Item {
+                    href = _itemHrefGenerator(m),
+                    links = _itemLinkGenerator(m),
+                    data = _itemDataGenerator(m)
+                });
+            }
+
+            if (_templateData != null) {
+                collection.template = new Template {data = _templateData};
+            }
+            
+            if (_queries != null) {
+                collection.queries = _queries;
+            }
+
+            return new CollectionJson {collection = collection};
+        }
+
+
+        public CollectionBuilder<TModel> Links(params Link[] links) {
+            _links = links;
+            return this;
+        } 
+
+        public CollectionBuilder<TModel> Items(
+            Func<TModel, string> href, 
+            Func<TModel, IEnumerable<Data>> data = null, 
+            Func<TModel, IEnumerable<Link>> links = null ) 
+        {
+            _itemHrefGenerator = href ?? (_ => null);
+            _itemDataGenerator = data ?? (_ => null);
+            _itemLinkGenerator = links ?? (_ => null);
+            return this;
+        } 
+
+        public CollectionBuilder<TModel> Template(params Data[] data) {
+            _templateData = data;
+            return this;
+        } 
+
+        public CollectionBuilder<TModel> Queries(params Query[] queries) {
+            _queries = queries;
+            return this;
+        }
+
+        public CollectionBuilder(string href, string version) {
+            _href = href;
+            _version = version;
+        }
+
+        private string _href;
+        private string _version;
+        private IEnumerable<Link> _links;
+        private Func<TModel, string> _itemHrefGenerator = (_ => null);
+        private Func<TModel, IEnumerable<Data>> _itemDataGenerator = (_ => null);
+        private Func<TModel, IEnumerable<Link>> _itemLinkGenerator = (_ => null);
+        private IEnumerable<Data> _templateData;
+        private IEnumerable<Query> _queries;
+    }
 }
---
+++ test/HyperNotes.CollectionJson.Test/CollectionBuilderTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using FluentAssertions;
+using Ploeh.AutoFixture;
+using Xunit;
+
+namespace HyperNotes.CollectionJson.Test {
+    public class CollectionBuilderTest {
+        public CollectionBuilderTest() {
+            collectionBuilder = DefineCollection.For<TestModel>(CollectionHref);
+        }
+
+        public class Build_method {
+            [Fact]
+            public void should_return_a_new_collectionJson_instance() {
+                var collection = collectionBuilder
+            }
+        }
+
+        private const string CollectionHref = "http://example.com/collection";
+        private CollectionBuilder<TestModel> collectionBuilder;
+    }
+
+    public class DefineCollectionTest {
+        public class For_method {
+
+            [Fact]
+            public void should_return_a_builder_for_the_specified_model_class() {
+                var builder = DefineCollection.For<TestModel>("http://example.com/collection");
+
+                builder.Should().BeOfType<CollectionBuilder<TestModel>>();
+            }
+        }
+    }
+
+    public class TestModel {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+
+}
