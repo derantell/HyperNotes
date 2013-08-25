7804b9a23c50c49950b0da1f8ba36b0072ac9c06
David Tellander <dtellander@gmail.com>
2013-08-25 20:48:46 +0200

    Added root module

---
+++ src/HyperNotes.Api/Root/ApiRootModule.cs
@@ -0,0 +1,11 @@
+﻿using Nancy;
+
+namespace HyperNotes.Api.Root {
+    public class ApiRootModule : NancyModule {
+        public ApiRootModule() : base("/") {
+            Get["/"] = _ => Negotiate
+                                .WithStatusCode(HttpStatusCode.OK)
+                                .WithView("Root/Representations/ApiRoot");
+        }
+    }
+}
